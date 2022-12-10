db441e7584e0c462d7bc611b4214252a00e9a7c2
ap-atul <atulpatare99@gmail.com>
2022-12-10 12:16:35 +0530

    added blue basic theme to start with

--- app/src/main/java/org/ranobe/ranobe/ui/library/Library.java
+++ app/src/main/java/org/ranobe/ranobe/ui/library/Library.java
@@ -1,12 +1,11 @@
 package org.ranobe.ranobe.ui.library;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import org.ranobe.ranobe.R;
 
