b060b7deeca3a7d2bc0af688426e37517eac9c15
ap-atul <atulpatare99@gmail.com>
2022-12-11 13:03:17 +0530

    added function to load instance of source with class ref

--- app/src/main/java/org/ranobe/ranobe/sources/SourceManager.java
+++ app/src/main/java/org/ranobe/ranobe/sources/SourceManager.java
@@ -2,13 +2,26 @@ package org.ranobe.ranobe.sources;
 
 import org.ranobe.ranobe.sources.en.ReadLightNovel;
 
+import java.util.HashMap;
+
 public class SourceManager {
     public static Source getSource(int sourceId) {
         try {
-            return ReadLightNovel.class.newInstance();
-        } catch (IllegalAccessException | InstantiationException e) {
+            Class<?> klass = getSources().get(sourceId);
+            if(klass == null) {
+                throw new ClassNotFoundException("Source not found with source id : " + sourceId);
+            }
+            return (Source) klass.newInstance();
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public static HashMap<Integer, Class<?>> getSources () {
+        HashMap<Integer, Class<?>> sources = new HashMap<>();
+        sources.put(1, ReadLightNovel.class);
+
+        return sources;
     }
 }
