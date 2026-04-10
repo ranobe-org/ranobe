31b14e7c8af166d021016cc1e88e857e34f22d3a
atul <atulpatare99@gmail.com>
2026-04-10 20:08:44 +0530

    code reformat

--- app/src/main/java/org/ranobe/ranobe/ui/library/Library.java
+++ app/src/main/java/org/ranobe/ranobe/ui/library/Library.java
@@ -68,8 +68,14 @@ public class Library extends Fragment implements NovelAdapter.OnNovelItemClickLi
         binding.toolbar.setOnMenuItemClickListener(this);
 
         binding.searchInput.addTextChangedListener(new TextWatcher() {
-            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
             @Override
             public void afterTextChanged(Editable s) {
                 applyFilterAndSort(s.toString());
