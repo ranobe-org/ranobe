5c369897ea396916b85bf946be7c1e550dd62cd9
atul <atulpatare99@gmail.com>
2026-09-08 12:27:18 +0530

    upgrade ranobehub

--- app/src/main/java/org/ranobe/ranobe/util/SourceUtils.java
+++ app/src/main/java/org/ranobe/ranobe/util/SourceUtils.java
@@ -2,6 +2,8 @@ package org.ranobe.ranobe.util;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class SourceUtils {
     public static Long generateId(String url) {
@@ -31,6 +33,19 @@ public class SourceUtils {
     public static String getDate(int timestamp) {
         try {
             return SimpleDateFormat.getDateTimeInstance().format(new Date(timestamp * 1000L));
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static String parseIsoDate(String timestamp) {
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.US);
+            inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+            Date date = inputFormat.parse(timestamp);
+            if (date == null) return "";
+            return SimpleDateFormat.getDateTimeInstance().format(date);
         } catch (Exception e) {
             return "";
         }
